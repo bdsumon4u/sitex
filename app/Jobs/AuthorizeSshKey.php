d02a360bea0edac01c0861aa61b905bb78f89503
Sumon Ahmed <bdsumon4u@gmail.com>
2026-01-28 18:41:33 +0600

    feat: enhance SSH key authorization and site deployment process with FTP integration

--- app/Jobs/AuthorizeSshKey.php
+++ app/Jobs/AuthorizeSshKey.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Models\Site;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
@@ -12,38 +13,18 @@ class AuthorizeSshKey implements ShouldQueue
 {
     use Queueable;
 
+    private Filesystem $sourceFtp;
+
+    private Filesystem $targetFtp;
+
     /**
      * Create a new job instance.
      */
     public function __construct(
         private Site $site,
     ) {
-        $storage = Storage::build([
-            'driver' => 'ftp',
-            'host' => $this->site->hosting->server->ip,
-            'username' => $this->site->hosting->username,
-            'password' => $this->site->hosting->password,
-
-            // Optional but recommended
-            'port' => $this->site->hosting->server->ftp_port,
-            'root' => env('FTP_ROOT', '/'),
-            'passive' => true,
-            'ssl' => false,
-            'timeout' => 30,
-        ]);
-
-        $storage->put('.ssh/HOTASH', Storage::disk('local')->get('HOTASH'), 'private');
-        $storage->put('.ssh/HOTASH.pub', Storage::disk('local')->get('HOTASH.pub'), 'private');
-
-        Log::info('Authorizing SSH key for '.$this->site->domain);
-        $data = $this->site->hosting->cPanel('SSH', 'authkey', [
-            'key' => 'HOTASH',
-            'action' => 'authorize',
-        ], 'cpanelresult');
-
-        if (array_key_exists('error', $data)) {
-            Log::error('Failed to authorize SSH key: '.$data['error']);
-        }
+        $this->sourceFtp = $this->buildFtpStorage($this->site->parent);
+        $this->targetFtp = $this->buildFtpStorage($this->site);
     }
 
     /**
@@ -51,6 +32,46 @@ class AuthorizeSshKey implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $this->copySshKeyViaFtp($this->sourceFtp);
+        $this->copySshKeyViaFtp($this->targetFtp);
+
+        $this->authorize($this->site->parent);
+        $this->authorize($this->site);
+    }
+
+    private function buildFtpStorage(Site $site): Filesystem
+    {
+        return Storage::build([
+            'driver' => 'ftp',
+            'host' => $site->hosting->server->ip,
+            'username' => $site->hosting->username,
+            'password' => $site->hosting->password,
+
+            // Optional but recommended
+            'port' => $site->hosting->server->ftp_port,
+            'root' => env('FTP_ROOT', '/'),
+            'passive' => true,
+            'ssl' => false,
+            'timeout' => 30,
+        ]);
+    }
+
+    private function copySshKeyViaFtp(Filesystem $ftp): void
+    {
+        $ftp->put('.ssh/HOTASH', Storage::disk('local')->get('HOTASH'), 'private');
+        $ftp->put('.ssh/HOTASH.pub', Storage::disk('local')->get('HOTASH.pub'), 'private');
+    }
+
+    private function authorize(Site $site): void
+    {
+        Log::info('Authorizing SSH key for '.$site->domain);
+        $data = $site->hosting->cPanel('SSH', 'authkey', [
+            'key' => 'HOTASH',
+            'action' => 'authorize',
+        ], 'cpanelresult');
+
+        if (array_key_exists('error', $data)) {
+            Log::error('Failed to authorize SSH key: '.$data['error']);
+        }
     }
 }
--- app/Jobs/CopySiteFromParent.php
+++ app/Jobs/CopySiteFromParent.php
@@ -2,10 +2,12 @@
 
 namespace App\Jobs;
 
+use App\Enums\SiteStatus;
 use App\Models\Site;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
+use Spatie\Ssh\Ssh;
 
 class CopySiteFromParent implements ShouldQueue
 {
@@ -26,5 +28,36 @@ class CopySiteFromParent implements ShouldQueue
     public function handle(): void
     {
         Log::info('Deploying site '.$this->site->name.' to '.$this->site->domain);
+        $process = Ssh::create($this->site->hosting->username, $this->site->hosting->server->ip)
+            ->usePrivateKey(storage_path('app/ssh/HOTASH'))
+            ->disablePasswordAuthentication()
+            ->disableStrictHostKeyChecking()
+            ->enableQuietMode()
+            ->setTimeout(1000)
+            ->execute([
+                'cd '.$this->site->parent->directory,
+                './copy.sh '.collect([
+                    '-s' => $this->site->name,
+                    '-d' => $this->site->domain,
+                    '-h' => $this->site->hosting->server->ip,
+                    '-u' => $this->site->hosting->username,
+                    '-db' => $this->site->hosting->database_name,
+                    '-dbu' => $this->site->hosting->database_user,
+                    '-dbp' => $this->site->hosting->database_pass,
+                    '-mu' => $this->site->hosting->email_username,
+                    '-mp' => $this->site->hosting->email_password,
+                    '-dr' => $this->site->hosting->directory,
+                ])
+                    ->flatMap(fn ($val, $key) => [$key, '"'.$val.'"'])
+                    ->implode(' '),
+            ]);
+
+        if (! $process->isSuccessful()) {
+            $this->site->update(['status' => SiteStatus::DEPLOY_FAILED]);
+            throw new \Exception($process->getErrorOutput());
+        }
+
+        $this->site->update(['status' => SiteStatus::SITE_ACTIVE]);
+        Log::info('Site '.$this->site->name.' deployed successfully to '.$this->site->domain);
     }
 }
