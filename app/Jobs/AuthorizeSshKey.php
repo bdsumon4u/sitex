1ec537558807373a0d45185cac403cc5bec7e488
Sumon Ahmed <bdsumon4u@gmail.com>
2026-01-28 17:54:02 +0600

    feat: implement SSH key authorization in AuthorizeSshKey job with FTP storage integration

--- app/Jobs/AuthorizeSshKey.php
+++ app/Jobs/AuthorizeSshKey.php
@@ -6,6 +6,7 @@ use App\Models\Site;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class AuthorizeSshKey implements ShouldQueue
 {
@@ -17,6 +18,23 @@ class AuthorizeSshKey implements ShouldQueue
     public function __construct(
         private Site $site,
     ) {
+        $storage = Storage::build([
+            'driver' => 'ftp',
+            'host' => $this->site->hosting->server->ip,
+            'username' => $this->site->hosting->username,
+            'password' => $this->site->hosting->password,
+
+            // Optional but recommended
+            'port' => $this->site->hosting->server->ftp_port,
+            'root' => env('FTP_ROOT', '/'),
+            'passive' => true,
+            'ssl' => false,
+            'timeout' => 30,
+        ]);
+
+        $storage->put('.ssh/HOTASH', Storage::disk('local')->get('HOTASH'), 'private');
+        $storage->put('.ssh/HOTASH.pub', Storage::disk('local')->get('HOTASH.pub'), 'private');
+
         Log::info('Authorizing SSH key for '.$this->site->domain);
         $data = $this->site->hosting->cPanel('SSH', 'authkey', [
             'key' => 'HOTASH',
