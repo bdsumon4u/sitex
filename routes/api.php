b3422c3e59f51a43ede379dc7c0781c8d3121472
Sumon Ahmed <bdsumon4u@gmail.com>
2026-05-11 11:55:59 +0600

    fix: remove throttling middleware from sites endpoint

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::get('/get-service-id/{domain}', function (string $domain) {
     ], 404);
 });
 
-Route::middleware('throttle:20,5')->get('sites', function (Request $request) {
+Route::get('sites', function (Request $request) {
     return Site::query()
         ->when($request->has('status'), fn ($query) => $query->where('status', $request->input('status')))
         ->get(['id', 'domain', 'status']);
